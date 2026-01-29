0fc09f6428252eedadfae9b20dc374f4c8cdc3f1
terzurumluoglu <toprakerzurumluoglu@gmail.com>
2026-01-30 00:22:54 +0300

    added redis

--- libs/api/core/src/register.ts
+++ libs/api/core/src/register.ts
@@ -7,8 +7,10 @@ import { correlationIdPlugin } from './plugins/correlation-id.plugin';
 import { errorHandlerPlugin } from './plugins/error-handler.plugin';
 import { notFoundPlugin } from './plugins/not-found.plugin';
 import { rateLimitPlugin } from './plugins/rate-limit';
+import { redisPlugin } from "@micro-services/api-redis";
 
 export async function registerCorePlugins(app: FastifyInstance) {
+  await app.register(redisPlugin);
   await app.register(rateLimitPlugin);
   await app.register(correlationIdPlugin);
   await app.register(errorHandlerPlugin);
